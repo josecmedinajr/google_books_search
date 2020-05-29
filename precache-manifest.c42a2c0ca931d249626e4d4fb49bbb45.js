3474505ce4ffd25f6bb5f00fd2041e2d944d48a5
Jose Medina <yanoya10@hotmail.com>
2020-05-29 15:54:36 -0400

    Updates

---
+++ precache-manifest.c42a2c0ca931d249626e4d4fb49bbb45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc3e7f00e573fb851b93918ec35a4460",
+    "url": "/google_books_search/index.html"
+  },
+  {
+    "revision": "026a8dd6d2440dc4daed",
+    "url": "/google_books_search/static/css/main.f35adeb2.chunk.css"
+  },
+  {
+    "revision": "f10adf66cc4501eb528f",
+    "url": "/google_books_search/static/js/2.0da8e7ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/google_books_search/static/js/2.0da8e7ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "026a8dd6d2440dc4daed",
+    "url": "/google_books_search/static/js/main.df6a18ad.chunk.js"
+  },
+  {
+    "revision": "5298c0aab4463845c569",
+    "url": "/google_books_search/static/js/runtime-main.b9e1e93b.js"
+  }
+]);
